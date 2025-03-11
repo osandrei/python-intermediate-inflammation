ed3ee48c569724ebb9ecb6df720ba08f24b124f3
osandrei <andrei.ludvig.osipov@gmail.com>
2025-03-11 13:34:58 +0000

    Fix styling and update the docstrings

--- inflammation/models.py
+++ inflammation/models.py
@@ -10,25 +10,37 @@ and each column represents a single day across all patients.
 import numpy as np
 
 
-def load_csv(filename):  
+def load_csv(filename):
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
+    :returns: numpy array with data
     """
     return np.loadtxt(fname=filename, delimiter=',')
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+    
+    :param data: a 2D numpy array with inflamation data
+    :returns: an array of daily mean data
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    
+    :param data: a 2D numpy array with inflamation data
+    :returns: an array of daily max data
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    
+    :param data: a 2D numpy array with inflamation data
+    :returns: an array of daily min data
+    """
     return np.min(data, axis=0)
-
