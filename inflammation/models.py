698653ec8a39e8debd084b74185d7c48573207d7
osandrei <andrei.ludvig.osipov@gmail.com>
2025-03-18 14:11:03 +0000

    Add normalization and a test for it

--- inflammation/models.py
+++ inflammation/models.py
@@ -44,3 +44,8 @@ def daily_min(data):
     :returns: an array of daily min data
     """
     return np.min(data, axis=0)
+
+def patient_normalise(data):
+    """Normalise patient data from a 2D inflammation data array."""
+    max = np.max(data, axis=0)
+    return data / max[:, np.newaxis]
--- tests/test_models.py
+++ tests/test_models.py
@@ -6,6 +6,7 @@ import numpy.testing as npt
 import pytest
 
 from inflammation.models import daily_mean
+from inflammation.models import patient_normalise
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""  
@@ -60,3 +61,16 @@ def test_daily_mean_empty():
 def test_daily_mean(test,expected):
     """Test that the mean function works for an array of zeros and positive integers."""
     npt.assert_equal(daily_mean(np.array(test)),np.array(expected))
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+    ])
+def test_patient_normalise(test, expected):
+    """Test normalisation works for arrays of one and positive integers.
+       Test with a relative and absolute tolerance of 0.01."""
+
+    result = patient_normalise(np.array(test))
+    npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
+    
